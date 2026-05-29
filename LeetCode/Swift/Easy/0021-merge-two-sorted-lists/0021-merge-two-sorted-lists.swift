7a25fa72eef87f720e5b1b871144e0493e8d1f11
JaeWoong Eum <98391474+woolnd@users.noreply.github.com>
2026-05-30 01:02:48 +0900

    Time: 0 ms (100%), Space: 19.3 MB (83.44%) - LeetHub

---
+++ LeetCode/Swift/Easy/0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.swift
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init() { self.val = 0; self.next = nil; }
+ *     public init(_ val: Int) { self.val = val; self.next = nil; }
+ *     public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+ * }
+ */
+class Solution {
+    func mergeTwoLists(_ list1: ListNode?, _ list2: ListNode?) -> ListNode? {
+        let dummy = ListNode(-1)
+        var current = dummy
+
+        var l1 = list1
+        var l2 = list2
+
+        while let n1 = l1, let n2 = l2 {
+            if n1.val <= n2.val {
+                current.next = n1
+                l1 = n1.next
+            } else {
+                current.next = n2
+                l2 = n2.next
+            }
+
+            current = current.next!
+        }
+
+        current.next = (l1 != nil) ? l1 : l2
+
+        return dummy.next
+    }
+}
