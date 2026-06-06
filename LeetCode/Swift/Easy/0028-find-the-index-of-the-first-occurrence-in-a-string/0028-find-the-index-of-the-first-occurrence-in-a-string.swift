ad6f97cf46c7e3ad206fb706ba7d472795afbf55
JaeWoong Eum <98391474+woolnd@users.noreply.github.com>
2026-06-06 22:53:09 +0900

    Time: 792 ms (7.88%), Space: 20.2 MB (19.86%) - LeetHub

---
+++ LeetCode/Swift/Easy/0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.swift
@@ -0,0 +1,9 @@
+class Solution {
+    func strStr(_ haystack: String, _ needle: String) -> Int {
+        if let range = haystack.range(of: needle) {
+            return haystack.distance(from: haystack.startIndex, to: range.lowerBound)
+        }
+
+        return -1
+    }
+}
