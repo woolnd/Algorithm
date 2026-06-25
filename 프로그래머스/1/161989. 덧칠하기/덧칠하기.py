1dc5f5ba13ce308f57e1189c5441930cff529faa
JaeWoong Eum <98391474+woolnd@users.noreply.github.com>
2026-06-25 09:36:16 +0900

    [level 1] Title: 덧칠하기, Time: 1.84 ms, Memory: 13.3 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.py
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.py
@@ -2,10 +2,9 @@ def solution(n, m, section):
     answer = 1
     painted = section[0] + m - 1
     
-    if len(section) > 1:
-        for s in section[1:]:
-            if s > painted:
-                answer += 1
-                painted = s + m - 1
+    for s in section[1:]:
+        if s > painted:
+            answer += 1
+            painted = s + m - 1
     
     return answer
