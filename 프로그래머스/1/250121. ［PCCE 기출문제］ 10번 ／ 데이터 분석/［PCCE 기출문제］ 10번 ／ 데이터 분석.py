b2a360e566d61e1e13e764a83d4ef8312de936d6
JaeWoong Eum <98391474+woolnd@users.noreply.github.com>
2026-06-28 00:27:25 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 0.09 ms, Memory: 11.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.py
@@ -0,0 +1,29 @@
+def solution(data, ext, val_ext, sort_by):
+    answer = [[]]
+
+    if ext == 'code':
+        mask = 0
+    elif ext == 'date':
+        mask = 1
+    elif ext == 'maximum':
+        mask = 2
+    else:
+        mask = 3
+        
+    for info in data:
+        if info[mask] < val_ext:
+            answer.append(info)
+            
+    if sort_by == 'code':
+        mask = 0
+    elif sort_by == 'date':
+        mask = 1
+    elif sort_by == 'maximum':
+        mask = 2
+    else:
+        mask = 3
+        
+    answer = list(filter(None, answer))
+    answer.sort(key=lambda x: x[mask])
+    
+    return answer
