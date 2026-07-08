56a77c3285c43ee49aa814a5024fe0820835f98f
JaeWoong Eum <98391474+woolnd@users.noreply.github.com>
2026-07-08 20:45:50 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 0.02 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.py
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.py
@@ -1,52 +1,27 @@
-def solution(video_len, pos, op_start, op_end, commands):
-    
-    video_split = video_len.split(":")
-    v_h = int(video_split[0]) * 60
-    v_m = int(video_split[1])
-    v_s = v_h + v_m
-    
-    pos_split = pos.split(":")
-    h = int(pos_split[0]) * 60
-    m = int(pos_split[1])
-    s = h + m
-    
-    op_start_split = op_start.split(":")
-    op_s_h = int(op_start_split[0]) * 60
-    op_s_m = int(op_start_split[1])
-    op_s_s = op_s_h + op_s_m
-    
-    op_end_split = op_end.split(":")
-    op_e_h = int(op_end_split[0]) * 60
-    op_e_m = int(op_end_split[1])
-    op_e_s = op_e_h + op_e_m
-    
-    
-    for command in commands:
-        if (op_s_s<=s) and (s<=op_e_s):
-            s = op_e_s
-        
-        if command == 'next':
-            s += 10
-            
-            if s >= v_s:
-                s = v_s
-                
-        elif command == 'prev':
-            s -= 10
+def to_sec(t):
+    m, s = map(int, t.split(":"))
+    return m * 60 + s
 
-            if s <= 0:
-                s = 0
-    
-    if (op_s_s<=s) and (s<=op_e_s):
-        s = op_e_s
-            
-    if s  >= 60:
-        h = s // 60
-        m = s % 60
-        
-        result_h = f"{h}" if h>= 10 else f"0{h}"
-        result_m = f"{m}" if m>= 10 else f"0{m}"
-    else:
-        result_h = "00"
-        result_m = f"{s}" if s >= 10 else f"0{s}"
-    return f"{result_h}:{result_m}"
+def to_str(sec):
+    m, s = divmod(sec, 60)
+    return f"{m:02d}:{s:02d}"
+
+def solution(video_len, pos, op_start, op_end, commands):
+    v = to_sec(video_len)
+    s = to_sec(pos)
+    op_s = to_sec(op_start)
+    op_e = to_sec(op_end)
+
+    for command in commands:
+        if op_s <= s <= op_e:
+            s = op_e
+
+        if command == 'next':
+            s = min(s + 10, v)
+        elif command == 'prev':
+            s = max(s - 10, 0)
+
+    if op_s <= s <= op_e:
+        s = op_e
+
+    return to_str(s)
